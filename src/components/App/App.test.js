240f4e54f817e4df04d88e72ebe9a24a81d525b2
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-03 16:32:30 -0600

    Set up testing packages

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App',() => {
+  it('should update state when changeNews is called', () => {
+    // setup 
+
+    const wrapper = shallow(<App />);
+
+    const mockArticle = [ {
+      id: 1,
+      headline: 'Spider-Man Will Remain in the Marvel Cinematic Universe',
+      img: 'https://static01.nyt.com/images/2019/09/27/multimedia/27xp-spiderman/27xp-spiderman-superJumbo.jpg?quality=90&auto=webp',
+      description: 'The web slinger will be the only superhero with the ability to cross between Disney and Sony’s cinematic universes, according to Marvel Studios.',
+      url: 'https://www.nytimes.com/2019/09/27/business/media/spiderman-mcu-sony-marvel.html'
+    }]
+
+    const expected = {
+      news: mockArticle
+    }
+
+    // execution
+
+    wrapper.instance().changeNews(mockArticle);
+
+    // expectation
+
+    expect(wrapper.state('news')).toEqual(expected)
+  });
+
+})
+
