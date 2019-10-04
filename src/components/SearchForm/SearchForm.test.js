c9f179eb1bd1049713dbd7763a3b14f4b5af72b7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-03 21:33:01 -0600

    Second test for searchForm passes

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -5,8 +5,34 @@ import SearchForm from './SearchForm';
 describe('SearchForm', () => {
     let wrapper;
     const mockIdea = jest.fn();
+    const mockHeadline = 'Spider-man'
 
     beforeEach(() => {
-        wrapper = shallow(<SearchForm changeNews={mockIdea} findNews={} />);
+        wrapper = shallow(<SearchForm changeNews={mockIdea} findNews={mockHeadline} />);
+    })
+
+    it('should match snapshot',() => {
+      expect(wrapper).toMatchSnapshot();
+    })
+
+    it('should check if state has changed when invoking handleChange',() => {
+        // setup
+
+        let mockState = {
+            target: {
+                name: 'title',
+                value: 'Spidey senses'
+            }
+        };
+
+        let expected = 'Spidey senses';
+
+        // execution
+
+        wrapper.instance().handleChange(mockState);
+
+        // expectation
+
+        expect(wrapper.state('title')).toEqual(expected);
     })
 })
