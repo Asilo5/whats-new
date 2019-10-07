f7f363908ef449ecc26547e1e548de9f88334f3b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-06 23:11:22 -0600

    Updates

---
+++ precache-manifest.dc5fbe1617d1bebf92d2377d0e5b51ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ceedb1aa03e42ebe9d8409adc159d2e",
+    "url": "/whats-new/index.html"
+  },
+  {
+    "revision": "330c0c8d35f70c624972",
+    "url": "/whats-new/static/css/main.1f0a9097.chunk.css"
+  },
+  {
+    "revision": "aca56e2da29cec9c24bd",
+    "url": "/whats-new/static/js/2.8d4277c5.chunk.js"
+  },
+  {
+    "revision": "330c0c8d35f70c624972",
+    "url": "/whats-new/static/js/main.b9e4b3f7.chunk.js"
+  },
+  {
+    "revision": "1f94ce0c8e9eb67f44d3",
+    "url": "/whats-new/static/js/runtime-main.2216c931.js"
+  }
+]);
